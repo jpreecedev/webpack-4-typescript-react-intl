47eb20006d22b50c21cd98d09e735c9eb87f8656
Jon Preece <jonpreece@hotmail.co.uk>
2019-09-17 19:29:24 +0100

    Implemented fix as advised by React Intl team

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const { resolve } = require("path");
+const { resolve, join } = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
@@ -11,9 +11,14 @@ const config = {
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.(t|j)sx?$/,
         loader: ["awesome-typescript-loader?module=es6"],
-        exclude: () => false
+        include: [
+          join(__dirname, "src"),
+          join(__dirname, "node_modules/react-intl"),
+          join(__dirname, "node_modules/intl-messageformat"),
+          join(__dirname, "node_modules/intl-messageformat-parser")
+        ]
       },
       {
         test: /\.js$/,
